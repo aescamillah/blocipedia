bb1cc708f672bbade41d2c76f85c16f2765c545b
AEH <escamilla.a@gmail.com>
2016-01-13 16:20:07 -0600

    hmt 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  has_many :wikis, dependent: :destroy, through: :collaborators
+  has_many :wikis, through: :collaborators
   has_many :collaborators
   after_initialize :init
 
